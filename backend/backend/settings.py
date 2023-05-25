f1b111223909c892f66b836ab0d8f37049cc2bae
Olga Shimchenko <os@maxmarine.ru>
2023-05-25 20:51:13 +0300

    add deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []  # ['51.250.105.112', '127.0.0.1', 'localhost', 'taskolkio.hopto.org']
+ALLOWED_HOSTS = ['51.250.105.112', '127.0.0.1', 'localhost', 'taskolkio.hopto.org']
 
 
 # Application definition
